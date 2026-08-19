dacfe518e7dfb86b551f36441ef6c075f64319cf
Norbu Sherpa <nobusherpaa@gmail.com>
2026-08-19 07:32:05 +0545

    new change from dev

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,7 +3,7 @@ import styles from './about.module.css'
 // Inline CSS 
 // CSS Modules
 // normal css -> css file for every page/component file name -> .module.css
-
+// new update from development
 // style component
 // tailwind
 
